480ca24b445272f03c4fbbe43b04f5a0384fffbb
Slava <Dogggy9@gmail.com>
2025-02-02 18:05:08 +0300

    Реализовали компонент UserMove

--- src/doggy/tictactoe/component/UserMove.java
+++ src/doggy/tictactoe/component/UserMove.java
@@ -16,14 +16,50 @@
 
 package doggy.tictactoe.component;
 
+import doggy.tictactoe.model.Cell;
 import doggy.tictactoe.model.GameTable;
+
+import java.util.Scanner;
 
 /**
  * @author doggy
  * @link
  */
 public class UserMove {
-    public void make(final GameTable gameTable) {
+    private final char[][] mapping = {
+            {'7', '8', '9'},
+            {'4', '5', '6'},
+            {'1', '2', '3'}
+    };
 
+    public void make(final GameTable gameTable) {
+        while (true) {
+            final Cell cell = getUserInput();
+            if (gameTable.isEmpty(cell)) {
+                gameTable.setSign(cell, 'X');
+                return;
+            } else {
+                System.out.println("Ячейка не пуста");
+            }
+        }
+    }
+
+    private Cell getUserInput() {
+        while (true) {
+            System.out.println("введите от 1 до 9");
+            final String userInput = new Scanner(System.in).nextLine();
+            if (userInput.length() == 1) {
+                char ch = userInput.charAt(0);
+                if (ch >= '1' && ch <= '9') {
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 3; j++) {
+                            if (mapping[i][j] == ch) {
+                                return new Cell(i, j);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
