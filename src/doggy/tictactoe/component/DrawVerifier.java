46fa13b86b12bf83404b4ff2e010694fa7026561
Slava <Dogggy9@gmail.com>
2025-02-02 18:16:40 +0300

    Реализовали компонент DrawVerifier

--- src/doggy/tictactoe/component/DrawVerifier.java
+++ src/doggy/tictactoe/component/DrawVerifier.java
@@ -16,6 +16,7 @@
 
 package doggy.tictactoe.component;
 
+import doggy.tictactoe.model.Cell;
 import doggy.tictactoe.model.GameTable;
 
 /**
@@ -24,6 +25,13 @@ import doggy.tictactoe.model.GameTable;
  */
 public class DrawVerifier {
     public boolean isDraw(final GameTable gameTable) {
-        return false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (gameTable.isEmpty(new Cell(i, j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
